b3611a8200f8d9157d5e0fcd3922b36746e9f1a3
radouanMarour <radouanmr999@gmail.com>
2025-02-05 15:29:59 +0100

    changed base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
-const baseUrl = "http://localhost:5000/api"
+const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
 
 const API = axios.create({ baseURL: baseUrl });
 
